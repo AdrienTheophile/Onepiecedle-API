bc5b454e7520b423855a2ee0a26a312235954cb6
Adrien <didit92400@gmail.com>
2025-11-29 23:09:11 +0100

    Onepiecedle API

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import fs from "fs"
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const jsonString = fs.readFileSync("./personnages.json", "utf-8");
+const personnages = JSON.parse(jsonString);
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('API Onepiecedle 2.0 en ligne !')
+})
+
+app.get('/api/personnages', (req, res) => {
+    res.json(personnages)
+})
+
+app.listen(port, () => {
+    console.log(`Serveur lancé sur le port : ${port}`)
+})
